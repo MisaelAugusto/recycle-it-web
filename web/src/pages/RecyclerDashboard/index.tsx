62be351c5db8b6565d677616a6a22a37217e34fc
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-10-27 12:10:12 -0300

    Add initial components to recycler dashboard page

--- web/src/pages/RecyclerDashboard/index.tsx
+++ web/src/pages/RecyclerDashboard/index.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
+import { FiFolder } from 'react-icons/fi';
 
-import { Container, Content } from './styles';
+import {
+  Container,
+  SideBar,
+  Profile,
+  Menu,
+  MenuCategory,
+  MenuItem,
+  Content
+} from './styles';
 
 const RecyclerDashboard: React.FC = () => {
   return (
     <Container>
-      <Content />
+      <SideBar>
+        <Profile>
+          <img
+            src="https://avatars3.githubusercontent.com/u/30798460?s=460&v=4"
+            alt="Misael Augusto"
+          />
+          <p>Misael Augusto</p>
+        </Profile>
+        <Menu>
+          <MenuCategory>
+            <p>Recycling</p>
+            <MenuItem to="/">
+              <FiFolder />
+              <p>Projects</p>
+            </MenuItem>
+            <MenuItem to="/">
+              <FiFolder />
+              <p>Projects</p>
+            </MenuItem>
+            <MenuItem to="/">
+              <FiFolder />
+              <p>Projects</p>
+            </MenuItem>
+          </MenuCategory>
+          <MenuCategory>
+            <p>Recycling</p>
+            <MenuItem to="/">
+              <FiFolder />
+              <p>Projects</p>
+            </MenuItem>
+            <MenuItem to="/">
+              <FiFolder />
+              <p>Projects</p>
+            </MenuItem>
+            <MenuItem to="/">
+              <FiFolder />
+              <p>Projects</p>
+            </MenuItem>
+          </MenuCategory>
+        </Menu>
+      </SideBar>
+      <Content>
+        <h1>Content</h1>
+      </Content>
     </Container>
   );
 };
--- web/src/pages/RecyclerDashboard/styles.ts
+++ web/src/pages/RecyclerDashboard/styles.ts
@@ -1,14 +1,62 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import BackgroundImg from '../../assets/background.png';
 
 export const Container = styled.div`
   height: 100vh;
-  background: url(${BackgroundImg}) no-repeat center;
-  background-size: 114%;
+  display: flex;
+`;
+export const SideBar = styled.div`
+  width: 12.8rem;
+  color: #ffffff;
+  padding: 1rem;
+  background-color: #1dbc5c;
 `;
 
-export const Content = styled.div`
+export const Profile = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
+
+  img {
+    width: 8rem;
+    height: 8rem;
+    border-radius: 50%;
+  }
+
+  p {
+    margin-top: 0.5rem;
+    font-size: 1.2rem;
+  }
+`;
+
+export const Menu = styled.div``;
+
+export const MenuCategory = styled.div`
+  margin-top: 1rem;
+
+  > p {
+    opacity: 0.8;
+    font-size: 1rem;
+    margin-bottom: 0.5rem;
+  }
+`;
+
+export const MenuItem = styled(Link)`
+  color: white;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+
+  p {
+    font-size: 1rem;
+  }
+
+  svg {
+    margin: 0 1rem;
+  }
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  background-color: #f6f6f6;
 `;
