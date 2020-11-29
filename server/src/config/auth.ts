7f9897cc1cd0b7953f76009c36025208a4295167
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-11-29 00:18:15 -0300

    Add settings to authentication and upload

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '49f81f447588dc4258549b6f357164e0',
+    expiresIn: '1d'
+  }
+};
--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import CollectPointsRouter from './collect-points.routes';
-
-const routes = Router();
-
-routes.use('/collect-points', CollectPointsRouter);
-
-export default routes;
